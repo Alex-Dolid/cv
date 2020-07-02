46a3bc0e7346ce404578273bbc81cecf241c24c3
Alex Dolid <dolsd.sasha@mail.ru>
2020-07-02 23:05:38 +0300

    [refactored] code

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -6,8 +6,8 @@ import "styles/index.scss";
 import "./index.scss";
 
 $(document).ready(function () {
-    for (let page of pagesNames) {
-        $(`<a class="root__item" href="/${page}.html">${page}</a>`).appendTo('.root');
-    }
+  for (let page of pagesNames) {
+    $(`<a class="root__item" href="/${page}.html">${page}</a>`).appendTo('.root');
+  }
 });
 
